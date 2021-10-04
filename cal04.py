3a88538e74403fab1bcd03ddb287094ef591d0ec
Moeez1021 <moeez2019@namal.edu.pk>
2021-10-04 17:50:34 +0500

    Division Function

---
+++ cal04.py
@@ -0,0 +1,12 @@
+def sub(num1, num2):
+   return num1-num2
+def sum(num1, num2):
+   return num1+num2
+def mul(a,b):
+   return a*b
+def div(a,b):
+   return a/b
+print(div(6,2))
+print(sub(10,3))
+print(mul(8,5))
+print(sum(8,5))
